0ab71bed916c2723a5602476d77e3c2137382dcf
Nikita Sokov <nikitasokov1@gmail.com>
2024-08-17 19:24:22 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  const result = [];
+  const pairsOfBrackets = new Map(bracketsConfig);
+
+  for (const char of str) { 
+    if (pairsOfBrackets.get(result.at(-1)) == char) { 
+      result.pop(); 
+    } else { 
+      result.push(char);
+    }
+  }
+  return result.length === 0;
 }
